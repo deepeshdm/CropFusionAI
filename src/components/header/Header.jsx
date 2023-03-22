9089bccf58afa0fb078a4a59519e23f639a95896
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2023-03-22 23:56:22 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
                     <Link to="/fertilizer"> Fertilizer Recommendation </Link>
                 </li>
                 <li className="project-header-option">
-                    <Link to="#" onClick={() => { window.open('https://8080-797137136eb6451193a1f8c64a951490.patr.cloud/docs', '_blank'); }}>
+                    <Link to="#" onClick={() => { window.open('https://8080-797137136eb6451193a1f8c64a951490.onpatr.cloud/docs', '_blank'); }}>
                         API Docs
                     </Link>
                 </li>
